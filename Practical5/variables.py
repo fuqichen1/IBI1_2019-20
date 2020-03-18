aee2995893a52fa250e0ff3bdc2b5afd9dc8977c
Qichen FU <qichen.19@intl.zju.edu.cn>
2020-03-18 11:05:49 +0800

    add variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=123
+b=123456
+c=b%7
+print(c) 
+d=c/11
+e=d/13
+print(a>e)
+#a is greater than e. Because a>e is true. 
+
+X= True
+Y= True
+Z=(X and not Y)or(Y and not X) 
+print(Z)
+W=(X!=Y)
+print(W==Z)
