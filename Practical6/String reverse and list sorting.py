5a5d4f54f3590345de19814eaee1f28209c8f638
Qichen FU <qichen.19@intl.zju.edu.cn>
2020-03-18 15:20:11 +0800

    String reverse and list sorting.py

---
+++ Practical6/String reverse and list sorting.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 14:43:07 2020
+
+@author: 17426
+"""
+#input the list of 10 values
+gene_lengths=[9410,3944141,4442,105338,19149,76779,126550,36296,842,15981]
+#create a copy of the list
+gene_lengths_copy=gene_lengths[:]
+#sort the copied list
+gene_lengths_copy.sort()
+#the first one is the smallest, the last one is the biggest
+max=gene_lengths_copy[9]
+min=gene_lengths_copy[0]
+#remove the longest and shortest gene lengths
+gene_lengths.remove(max)
+gene_lengths.remove(min)
+#print the list with the longest and shortest gene lengths removed
+print(gene_lengths)
+
+#import matplotlib
+import matplotlib.pyplot as plt
+#set the boxplot
+plt.boxplot(gene_lengths,
+            vert=True,
+            whis=1.5,
+            patch_artist=True,
+            meanline=True,
+            showbox=True,
+            showcaps=True,
+            showfliers=True,
+            notch=False)
+#boxplot output
+plt.show()
